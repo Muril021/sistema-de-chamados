a9416043da65e5512729c74f61195b8ec9063d61
Murilo Nascimento <murilojesus659@gmail.com>
2024-04-22 16:40:54 -0300

    adding customers page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,7 @@ import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
 import Private from "./Private";
 import Profile from "../pages/Profile";
+import Customers from "../pages/Customers";
 
 const RoutesApp = () => {
   return (
@@ -14,6 +15,7 @@ const RoutesApp = () => {
 
       <Route path="/dashboard" element={<Private><Dashboard /></Private>} />
       <Route path="/profile" element={<Private><Profile /></Private>} />
+      <Route path="/customers" element={<Private><Customers /></Private>} />
     </Routes>
   )
 }
